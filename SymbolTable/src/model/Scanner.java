c970a6aa78ac262e25d70c6921c8a8eda70da1d1
zrsoo <46485193+zrsoo@users.noreply.github.com>
2022-10-31 17:42:40 +0200

    regex

--- SymbolTable/src/model/Scanner.java
+++ SymbolTable/src/model/Scanner.java
@@ -1,12 +1,47 @@
 package model;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Scanner {
+    private final static String IDENTIFIER_REGEX = "[a-z]+[0-9]{0,1}";
+    private final static String INTEGER_REGEX = "0$|(?!0)[+-]{0,1}[1-9]+[0]*";
+
+    private Set<String> separators = new HashSet<>();
 
     public Scanner()
     {
+        readSeparators("token.in");
+    }
 
+    private void readSeparators(String path)
+    {
+        File file = new File(path);
+
+        BufferedReader br;
+
+        try {
+            br = new BufferedReader(new FileReader(file));
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        String line;
+
+        try {
+            while ((line = br.readLine()) != null)
+            {
+                separators.add(line);
+            }
+        }
+        catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 
     public void scan(String path)
